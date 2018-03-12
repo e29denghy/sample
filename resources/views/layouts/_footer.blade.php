62f9f95dc246411f9ad7cd65c04b19b5386cf894
e29denghy <42734828@qq.com>
2018-03-12 12:36:13 +0000

    fix home page

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -2,13 +2,19 @@
   <footer class="footer">
     <small class="slogon">
       <img class="brand-icon" src="https://dn-phphub.qbox.me/uploads/images/201612/12/1/iq7WQc2iuW.png?imageView2/1/w/34/h/34">
-      <a href="https://laravel-china.org/courses">
+      <a href="#">
         刻意练习，每日精进
       </a>
     </small>
     <nav>
       <ul>
         <li><a href="{{ route('about') }}">关于</a></li>
+      </ul>
+      <ul>
+        <li><a href="Http://www.miitbeian.gov.cn/">粤ICP备18024712号</a></li>
+      </ul>
+      <ul>
+        <li><a href="{{ route('about') }}">©2018 denghy.cn</a></li>
       </ul>
     </nav>
   </footer>
